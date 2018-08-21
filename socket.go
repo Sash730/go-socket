f225025769d87ea491e6cc33535da03d703367e9
maksim-mukovnin <m.mukovnin@forma-pro.com>
2018-08-21 18:51:10 +0300

    refactoring
    Signed-off-by: maksim-mukovnin <m.mukovnin@forma-pro.com>

--- socket.go
+++ socket.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"github.com/gorilla/mux"
 	"github.com/Sash730/go-socket/controller"
+	"github.com/gorilla/handlers"
+	"os"
 )
 
 
@@ -19,6 +21,15 @@ func main() {
 	// HEALTH CHECK
 	r.HandleFunc("/socket.io", c.ViewReport)
 
+
+
+	// these two lines are important in order to allow access from the front-end side to the methods
+	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
+
+	router := handlers.LoggingHandler(os.Stdout, r)
+	router = handlers.CORS(allowedOrigins, allowedMethods)(router)
+
 	log.Println("Serving at localhost:5000...")
-	log.Fatal(http.ListenAndServe(":5000", nil))
+	log.Fatal(http.ListenAndServe(":5000", router))
 }
